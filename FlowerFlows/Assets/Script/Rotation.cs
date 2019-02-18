dced1ffa98622a8e745984e1dd1d49c224d3b0e8
HaroldSama <122599Run>
2019-02-18 01:34:07 -0500

    1. Initial Setup 2. Basic Map and Mechanism Setup

---
+++ FlowerFlows/Assets/Script/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float Speed;
+    private float _angle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_angle = new Quaternion(0, Speed, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation =  Quaternion.AngleAxis(_angle, Vector3.up);
+        _angle += Speed;
+    }
+}
---
+++ FlowerFlows/Assets/Script/Touch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour
+{
+    public static bool Jumpable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //print("Touch");
+        if (Jumpable && other.CompareTag("Hand") && transform.Find("Player") != null)
+        {
+            print("Jump");
+            transform.Find("Player").transform.SetParent(other.transform.parent, false);
+            Jumpable = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (Jumpable == false && other.CompareTag("Hand") && transform.Find("Player") != null)
+        {
+            Jumpable = true;            
+        }
+
+    }
+}
