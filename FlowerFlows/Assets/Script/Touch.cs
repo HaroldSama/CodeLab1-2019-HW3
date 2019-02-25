0b3f6d6a17df6ed00227c7fc17f4f750d71ecd99
HaroldSama <122599Run>
2019-02-25 00:39:10 -0500

    Add Sliding action

--- FlowerFlows/Assets/Script/Touch.cs
+++ FlowerFlows/Assets/Script/Touch.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class Touch : MonoBehaviour
 {
-    private GameObject target;
+    private float SlidingSpeed = 0.1f;
+    public GameObject target;
+    public GameObject Destination;
+    public GameObject Player;
     public static bool Jumpable;
+    public static bool Sliding;
+    private bool SlidingPrivate;
+    public Vector3 Way;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //print("Touch");
-        if (Jumpable == false && other.CompareTag("Hand") && transform.Find("Player") != null)
+        if (!Sliding && !Jumpable && other.CompareTag("Hand") && transform.Find("Player") != null)
         {
             print("Ready");
             target = other.gameObject;
+            Destination = target.transform.parent.gameObject;
             Jumpable = true;
         }
     }
@@ -40,11 +47,44 @@ public class Touch : MonoBehaviour
     {
         if (Jumpable && Input.GetKeyDown(KeyCode.Space) && transform.Find("Player") != null)
         {
-            transform.Find("Player").transform.SetParent(target.transform.parent, false);
+            Player = transform.Find("Player").gameObject;
+            Player.transform.parent = null;
+            //transform.Find("Player").transform.SetParent(target.transform.parent, false);
+            Sliding = true;
+            SlidingPrivate = true;
             Jumpable = false;
             target = null;
         }
 
-    }    
+        if (Sliding && SlidingPrivate)
+        {
+            Slide();
+        }
+
+    }
+
+    void Slide()
+    {
+        Way =  Destination.transform.position - Player.transform.position;
+        if (Way.magnitude > SlidingSpeed)
+        {
+            /*print("Player" + Player.transform.position);
+            print("Destination" + Destination.transform.position);
+            print(Way);
+            print(Way.magnitude);
+            print(Way / Way.magnitude * SlidingSpeed);*/
+            Player.transform.position += Way / Way.magnitude * SlidingSpeed;
+        }
+        else
+        {
+            print("Set");
+            Player.transform.SetParent(Destination.transform, true);
+            Player.transform.position = Destination.transform.position;
+            Player = null;
+            Destination = null;
+            Sliding = false;
+            SlidingPrivate = false;
+        }
+    }
     
 }
